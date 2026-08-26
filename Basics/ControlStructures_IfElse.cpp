ad864564ab9a868ccc0f1daca0bb69ff67f47a0b
Karan Rangwani <karanrangwani1990@gmail.com>
2026-08-26 20:35:42 +0530

    New Topic

---
+++ Basics/ControlStructures_IfElse.cpp
@@ -0,0 +1,32 @@
+#include<iostream> 
+#include<iomanip>
+using namespace std ; 
+int main(){
+  //  Selection control system - If else - ladder
+    int age ; 
+   cout<<"Tell me your age : "<<endl ; 
+    cin>>age ; 
+    /*if(age >= 18 && age <=70){
+        cout<<"You can vote !!"<<endl ; }
+        else if (age > 70){
+            cout<<"You can vote but need to verify your voter card !!"<<endl ;} 
+        else{
+            cout<<"You cant vote !!"<<endl ;
+        }*/
+       // Selection control system - Switch case
+       switch (age){
+        case 18 : 
+        cout<<"You can vote !!"<<endl ;
+        // code to be executed if expression = 1
+        break ; 
+        case 70 : 
+        cout<<"You can vote but need to verify your voter card !!"<<endl ;
+        // code to be executed if expression = 2
+        break ; 
+        case 17 :  
+        cout<<"You cant vote !!"<<endl ;
+        break ; 
+        default : 
+        cout<<"You are not eligible to vote !!"<<endl ;
+       }
+    }
